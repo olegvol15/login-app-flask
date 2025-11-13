c024a0c16cc1b2dab4dee5ab65e3519a748215cd
Oleg_Volostnykh <olegbasketball15@gmail.com>
2025-11-13 19:19:08 +0100

    Bug fixex

--- run.py
+++ run.py
@@ -1,6 +1,11 @@
-from app import create_app
 from dotenv import load_dotenv
-load_dotenv()
+
+load_dotenv()  # загружаем .env в os.environ
+
+from app import create_app
+
 app = create_app()
+
 if __name__ == "__main__":
     app.run()
+
